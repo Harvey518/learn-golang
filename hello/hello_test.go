33465e1409a223c4fd3d72ba90cee61770734f0b
齐耀辉 <qiyaohui@bitoffer.com>
2021-04-27 17:05:43 +0800

    添加分组测试，以及公共判断函数已增加代码可读性

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,29 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	// got := Hello("Chris")
+	// want := "Hello, Chris"
 	
-	if got != want {
-		t.Errorf("want '%q' got '%q'", want, got)
+	// if got != want {
+	// 	t.Errorf("want '%q' got '%q'", want, got)
+	// }
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T){
+		got := Hello("Jerry")
+		want := "Hello, Jerry"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when an enmpty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
 }
